063960887f5a4c3117507b29974b864b440c2ec3
Johnkhay <johnkingsleyegeonu@gmail.com>
2023-03-13 15:29:59 +0100

    added animation the button and a rolling globe animation

--- quotes.js
+++ quotes.js
@@ -19,7 +19,7 @@ const quotes = [
 "Nothin bad happens on a FRIDAY ....... MY NIGGAS (especially SLIME)",
 "The greatest glory in living lies not in never falling, but in rising every time we fall.........-Nelson Mandela",
 "The way to get started is to quit talking and begin doing........... -Walt Disney",
-"Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking. .........-Steve Jobs",
+"Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma , which is living with the results of other people's thinking. .........-Steve Jobs",
 "If life were predictable it would cease to be life, and be without flavor........... -Eleanor Roosevelt",
 "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.........-Oprah Winfrey",
 "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success...........-James Cameron",
@@ -49,12 +49,14 @@ const quoteContaner = document.querySelectorAll('#conDiv');
 const quoteText = document.querySelector('#quote');
 const geeButton = document.querySelector('#butt')
 const body = document.querySelector('body')
-
+const button = document.querySelector('button')
+const globe = document.querySelector('.globe')
 geeButton.addEventListener('click', () =>  {
     const randomQ = quotes[Math.floor(Math.random() * quotes.length)]
     const randomC = colors[Math.floor(Math.random() * colors.length)]
 
     quoteText.textContent = randomQ;
    body.style.backgroundColor = randomC;
-    
+   button.style.backgroundColor = randomC;
+  globe.style.backgroundColor = randomC;
 });
