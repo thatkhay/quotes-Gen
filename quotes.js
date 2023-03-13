37d76c7a096d0c23bb3f484dbe687c49f4503445
Johnkhay <johnkingsleyegeonu@gmail.com>
2023-03-13 14:08:18 +0100

    added more quotes to the array

--- quotes.js
+++ quotes.js
@@ -16,7 +16,15 @@ const quotes = [
 "Talent wins games, but teamwork wins the tournament...........Micheal Jordan",
 "opportunities do not  happen , you create them..........Chris Grosser",
 "Its never too late to be what you might have been.........George Eliot",
-"Nothin bad happens on a FRIDAY ....... MY NIGGAS (especially SLIME)"
+"Nothin bad happens on a FRIDAY ....... MY NIGGAS (especially SLIME)",
+"The greatest glory in living lies not in never falling, but in rising every time we fall.........-Nelson Mandela",
+"The way to get started is to quit talking and begin doing........... -Walt Disney",
+"Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking. .........-Steve Jobs",
+"If life were predictable it would cease to be life, and be without flavor........... -Eleanor Roosevelt",
+"If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.........-Oprah Winfrey",
+"If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success...........-James Cameron",
+"Life is what happens when you're busy making other plans.............-John Lennon",
+
 ];
 
 const colors = [
